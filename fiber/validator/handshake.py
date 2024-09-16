03defa68f1ef9e0bd0e13fd3a1044de91b793c8b
namoray <neuvaamor@proton.me>
2024-09-16 15:09:30 +0100

    generate nonces

--- fiber/validator/handshake.py
+++ fiber/validator/handshake.py
@@ -8,6 +8,7 @@ import os
 from fiber.miner.security import signatures
 import base64
 from substrateinterface import Keypair
+from fiber.validator.generate_nonce import generate_nonce
 from fiber.validator.security.encryption import public_key_encrypt
 from fiber import constants as bcst
 
@@ -36,7 +37,9 @@ async def perform_handshake(
     return symmetric_key_str, symmetric_key_uuid
 
 
-async def get_public_encryption_key(httpx_client: httpx.AsyncClient, server_address: str, timeout: int = 3) -> rsa.RSAPublicKey:
+async def get_public_encryption_key(
+    httpx_client: httpx.AsyncClient, server_address: str, timeout: int = 3
+) -> rsa.RSAPublicKey:
     response = await httpx_client.get(url=f"{server_address}/{bcst.PUBLIC_ENCRYPTION_KEY_ENDPOINT}", timeout=timeout)
     response.raise_for_status()
     data = encryption.PublicKeyResponse(**response.json())
@@ -57,11 +60,13 @@ async def send_symmetric_key_to_server(
     timeout: int = 3,
 ) -> bool:
     payload = {
-        "encrypted_symmetric_key": base64.b64encode(public_key_encrypt(public_key_encryption_key, symmetric_key)).decode("utf-8"),
+        "encrypted_symmetric_key": base64.b64encode(
+            public_key_encrypt(public_key_encryption_key, symmetric_key)
+        ).decode("utf-8"),
         "symmetric_key_uuid": symmetric_key_uuid,
         "ss58_address": keypair.ss58_address,
         "timestamp": time.time(),
-        "nonce": os.urandom(16).hex(),
+        "nonce": generate_nonce(),
     }
 
     signature = signatures.sign_message(keypair, signatures.construct_message_from_payload(payload))
